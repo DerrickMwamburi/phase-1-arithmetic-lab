73970fe2dcd351034ccab57eeb2bff93d702dc8d
Derrick Katute <derrick.katute@student.moringaschool.com>
2025-03-11 08:54:14 +0300

    commit change

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+// Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // 14 % 5 = 4
+
+let max = Math.max(1, 5, 10, 15, 20); // Finds the highest number in the set, which is 20
